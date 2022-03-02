5d647c675d620e23bb3892c43c83d65282b14e51
Nathan Nowack <thrast36@gmail.com>
2022-03-02 13:13:06 -0600

    update alreadyStored

--- pgn-etl/flow.py
+++ pgn-etl/flow.py
@@ -1,12 +1,8 @@
 from chessdotcom import get_player_game_archives
-from datetime import timedelta
 from io import StringIO
 from prefect import flow, task
 from prefect.deployments import DeploymentSpec
-from prefect.orion.schemas.schedules import IntervalSchedule
-from prefect.tasks import task_input_hash
-from prefect.task_runners import DaskTaskRunner
-from typing import Tuple
+from typing import List, Tuple
 import boto3, chess.pgn as pgn, pandas as pd, requests
 
 class Game(pgn.Game):
@@ -16,15 +12,14 @@ class Game(pgn.Game):
         self.df = pd.json_normalize(dict(self.game_obj['headers']))
         self.df['pgn'] = self.game_obj['variations']
 
-def alreadyStored(url: str, bucket: str, username: str) -> bool:
+def alreadyStored(bucket: str, username: str) -> List[str]:
     client = boto3.client('s3')
-    year, month = url.split('/')[-2:]
-    archive_date = f"{year}/{month}"
-    stored_months = [i['Key'].split('.')[0] for i in client.list_objects_v2(Bucket=bucket, Prefix=username)['Contents']]
-    return any([archive_date in i for i in stored_months])
+    try:
+        return [i['Key'].split('.')[0] for i in client.list_objects_v2(Bucket=bucket, Prefix=username)['Contents']]
+    except KeyError:
+        return []
 
-
-@task(cache_key_fn=task_input_hash)
+@task
 def get_games(url: str) -> Tuple[Game]:
     print(f"GET {url}")
     raw_games = requests.get(url).json()['games']
@@ -35,25 +30,30 @@ def load_games(games: Tuple[Game], base_path: str) -> None:
     df = pd.concat([game.df for game in games])
     year, month, _ = list(df['Date'])[0].split('.')
     print(f'storing games from month {month} of year {year}...')
-    filepath = f's3://{base_path}/archived-chess-games/{year}/{month}.parquet.gzip'
+    filepath = f's3://{base_path}/games/{year}/{month}.parquet.gzip'
     df.to_parquet(filepath, compression='gzip')
 
-@flow(name="Store chess.com users' games", task_runner=DaskTaskRunner(), version='1.0.0')
+@flow(name="Store chess.com users' games", version='1.0.0')
 def orca(S3_bucket: str) -> None:
-    for username in ['glen_bishop', 'n80n8']:
-        print(username)
 
+    for username in ['n80n8']:
+        print(f"Checking for games on chess.com from: {username}")
+
+        # list of URLs to GET months of games from
         archive_urls = get_player_game_archives(username=username).archives
 
-        user_games = [get_games(url) for url in archive_urls if not alreadyStored(url, S3_bucket, username)]
+        # use already stored if desired
+        stored_games = alreadyStored(S3_bucket, username)
+        path_from = lambda url: url.split('player/')[-1]
+        new_user_games = [get_games(url) for url in archive_urls if path_from(url) not in stored_games]
 
-        if len(user_games) == 0:
+        if len(new_user_games) == 0:
             print(f'No new months of games to load for {username}!')
             continue
 
-        print(f'Fetching {len(user_games) } new months of games from {username}..')
+        print(f'Fetching {len(new_user_games) } new months of games from {username}..')
 
-        for month in user_games:
+        for month in new_user_games:
             load_games(
                 games=month,
                 base_path=f"{S3_bucket}/{username}"
